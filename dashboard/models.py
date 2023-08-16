d95171ae30d58d0b07ca10c5c077808f4b338d69
Boopya <rikiyaeduarteyamamoto@gmail.com>
2023-08-16 15:11:06 +0800

    Create model for sensor data

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class SensorData(models.Model):
+    box_no = models.TextField()
+    mq2_value = models.IntegerField()
+    mq3_value = models.IntegerField()
+    mq7_value = models.IntegerField()
+    mq9_value = models.IntegerField()
+    mq135_value = models.IntegerField()
+    ph_value = models.IntegerField()
+    temp_value = models.IntegerField()
+    timestamp = models.DateTimeField()
+
+    def __str__(self):
+        return str(self.timestamp)
