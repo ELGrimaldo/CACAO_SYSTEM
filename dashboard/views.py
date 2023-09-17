3d6f34dad2b24e968fc019b5974ea6658f665446
Rikiya Yamamoto <rikiyaeduarteyamamoto@gmail.com>
2023-09-17 13:14:36 +0800

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,36 +4,11 @@ from django.views.decorators.csrf import csrf_exempt
 from .models import SensorData
 from datetime import datetime
 
-from .manageData import list_files, CacaoData
-
 # Create your views here.
 
 
 def index(request):
-    context = {'boxes':{}}
-
-    counter = 1
-    for file in list_files():
-        data = CacaoData(file)
-        data = data.get_data()
-        mq2 = list(data['mq2_smooth'].values())
-        mq3 = list(data['mq3_smooth'].values())
-        mq7 = list(data['mq7_smooth'].values())
-        mq9 = list(data['mq9_smooth'].values())
-        mq135 = list(data['mq135_smooth'].values())
-
-        
-        file = file.split('.')[0]
-        context['boxes'][f'Box{counter}'] = {
-            'MQ-2': mq2,
-            'MQ-3': mq3,
-            'MQ-7': mq7,
-            'MQ-9': mq9,    
-            'MQ-135': mq135
-            }
-        
-        counter +=1
-    return render(request, 'dashboard.html', context)
+    return render(request, 'dashboard.html')
 
 def boxes(request):
     return render(request, 'boxes.html')
