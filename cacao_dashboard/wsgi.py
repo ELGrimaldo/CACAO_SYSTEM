83776831a267acb1ba7edb908e68015747cb1faa
Boopya <rikiyaeduarteyamamoto@gmail.com>
2023-09-17 11:45:47 +0800

    Change cacao_dashboard_project to cacao_dashboard

--- cacao_dashboard/wsgi.py
+++ cacao_dashboard/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cacao_dashboard_project project.
+WSGI config for cacao_dashboard project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cacao_dashboard_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cacao_dashboard.settings')
 
 application = get_wsgi_application()
