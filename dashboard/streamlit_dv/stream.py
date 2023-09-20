815bce4e87caae2db92d6d7cb299a82bd5b089a6
Eljames <jc.grimaldo@mseuf.edu.ph>
2023-09-20 13:07:03 +0800

    Changed chart interface

--- dashboard/streamlit_dv/stream.py
+++ dashboard/streamlit_dv/stream.py
@@ -3,18 +3,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+class DataChart:
+    
+    def __init__(self) -> None:
+        pass
+
 def main():
 #     df = pd.read_csv('../Databox_1_smoothened.csv')
-    st.title("Streamlit App in Django")
+    
     
 
     chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
+    np.random.randn(20, 5),
+    columns=['MQ-2', 'MQ-3', 'MQ-7', 'MQ-9', 'MQ-135'])
 
+    st.title("Fermentation Box 1")
     st.line_chart(chart_data)
-
-        # Streamlit app code here
+    
+    st.title("Fermentation Box 2")
+    st.line_chart(chart_data)
+    
+    st.title("Fermentation Box 3")
+    st.line_chart(chart_data)
+    
+    st.title("Fermentation Box 4")
+    st.line_chart(chart_data)
+    
 
 if __name__ == "__main__":
     main()
