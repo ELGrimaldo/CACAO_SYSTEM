2a6b9ea89dab847d3a7df7e694edb45994c8982a
Rikiya Yamamoto <rikiyaeduarteyamamoto@gmail.com>
2023-09-29 14:19:18 +0800

    Update models.py

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,15 +1,49 @@
 from django.db import models
 
-class SensorData(models.Model):
-    box_no = models.TextField()
-    mq2_value = models.IntegerField()
-    mq3_value = models.IntegerField()
-    mq7_value = models.IntegerField()
-    mq9_value = models.IntegerField()
-    mq135_value = models.IntegerField()
-    ph_value = models.IntegerField()
-    temp_value = models.IntegerField()
-    timestamp = models.DateTimeField()
+class BoxOne(models.Model):
+    reading_id = models.AutoField(primary_key=True)
+    timestamp = models.DateTimeField(auto_now=True)
+    mq2 = models.IntegerField()
+    mq3 = models.IntegerField()
+    mq7 = models.IntegerField()
+    mq9 = models.IntegerField()
+    mq135 = models.IntegerField()
 
     def __str__(self):
-        return str(self.timestamp)
+        return str(self.reading_id)
+
+class BoxTwo(models.Model):
+    reading_id = models.AutoField(primary_key=True)
+    timestamp = models.DateTimeField(auto_now=True)
+    mq2 = models.IntegerField()
+    mq3 = models.IntegerField()
+    mq7 = models.IntegerField()
+    mq9 = models.IntegerField()
+    mq135 = models.IntegerField()
+
+    def __str__(self):
+        return str(self.reading_id)
+    
+class BoxThree(models.Model):
+    reading_id = models.AutoField(primary_key=True)
+    timestamp = models.DateTimeField(auto_now=True)
+    mq2 = models.IntegerField()
+    mq3 = models.IntegerField()
+    mq7 = models.IntegerField()
+    mq9 = models.IntegerField()
+    mq135 = models.IntegerField()
+
+    def __str__(self):
+        return str(self.reading_id)
+    
+class BoxFour(models.Model):
+    reading_id = models.AutoField(primary_key=True)
+    timestamp = models.DateTimeField(auto_now=True)
+    mq2 = models.IntegerField()
+    mq3 = models.IntegerField()
+    mq7 = models.IntegerField()
+    mq9 = models.IntegerField()
+    mq135 = models.IntegerField()
+
+    def __str__(self):
+        return str(self.reading_id)
