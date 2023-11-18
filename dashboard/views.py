0acdfe261dc35e2edfbafa8da909e1b966e738fd
Rikiya Yamamoto <rikiyaeduarteyamamoto@gmail.com>
2023-11-18 15:19:19 +0800

    Remove conditional statements on which box the data comes from
    Since the data will be coming only from one box, the conditional statements are not needed

--- dashboard/views.py
+++ dashboard/views.py
@@ -24,55 +24,29 @@ def base(request):
 def receive_data(request):
     """ Handles incoming sensor data from an ESP32. """
 
+    # If it's a POST request, then extract the HTTP request data (sensor reading)
     if request.method == 'POST':
-        box_no = request.POST.get('box_no')
         mq2 = request.POST.get('mq2')
         mq3 = request.POST.get('mq3')
         mq7 = request.POST.get('mq7')
         mq9 = request.POST.get('mq9')
         mq135 = request.POST.get('mq135')
 
-        if box_no == '1':
-            BoxOne.objects.create(mq2 = mq2,
-                                  mq3 = mq3,
-                                  mq7 = mq7,
-                                  mq9 = mq9,
-                                  mq135 = mq135)
-            
-        elif box_no == '2':
-            BoxTwo.objects.create(mq2 = mq2,
-                                  mq3 = mq3,
-                                  mq7 = mq7,
-                                  mq9 = mq9,
-                                  mq135 = mq135)
-            
-        elif box_no == '3':
-            BoxThree.objects.create(mq2 = mq2,
-                                    mq3 = mq3,
-                                    mq7 = mq7,
-                                    mq9 = mq9,
-                                    mq135 = mq135)
-            
-        elif box_no == '4':
-            BoxFour.objects.create(mq2 = mq2,
-                                   mq3 = mq3,
-                                   mq7 = mq7,
-                                   mq9 = mq9,
-                                   mq135 = mq135)
+        # Then, store sensor reading in database
+        SensorData.objects.create(mq2, mq3, mq7, mq9, mq135)
         
-        print("[SERVER LOG] Data received successfully from box no. " + box_no + ": \n" + 
+        # Log the received data in the server
+        print("[SERVER LOG] Data received: \n" + 
                             "MQ2: " + mq2 + ", " +
                             "MQ3: " + mq3 + ", " +
                             "MQ7: " + mq7 + ", " +
                             "MQ9: " + mq9 + ", " +
                             "MQ135: " + mq135)
         
-        return HttpResponse("[FROM SERVER] Your sent data from box no. " + box_no + ": \n" + 
-                            "MQ2: " + mq2 + ", " +
-                            "MQ3: " + mq3 + ", " +
-                            "MQ7: " + mq7 + ", " +
-                            "MQ9: " + mq9 + ", " +
-                            "MQ135: " + mq135)
+        # Finally, return a success message
+        return HttpResponse("[FROM SERVER] Data received successfully")
+    
+    # If it's not a POST request, return an error message
     else:
         return HttpResponse("[FROM SERVER] Invalid request method")
 
