ffc43f9053680eb423c205ac92af198e7534c066
Rikiya Yamamoto <rikiyaeduarteyamamoto@gmail.com>
2023-11-18 15:12:02 +0800

    Use only one class to store sensor data
    Made it only one class that will store sensor data because only one box will be used for fermenting again

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,42 +1,7 @@
 from django.db import models
 
-class BoxOne(models.Model):
-    reading_id = models.AutoField(primary_key=True)
-    timestamp = models.DateTimeField(auto_now=True)
-    mq2 = models.IntegerField()
-    mq3 = models.IntegerField()
-    mq7 = models.IntegerField()
-    mq9 = models.IntegerField()
-    mq135 = models.IntegerField()
-
-    def __str__(self):
-        return str(self.reading_id)
-
-class BoxTwo(models.Model):
-    reading_id = models.AutoField(primary_key=True)
-    timestamp = models.DateTimeField(auto_now=True)
-    mq2 = models.IntegerField()
-    mq3 = models.IntegerField()
-    mq7 = models.IntegerField()
-    mq9 = models.IntegerField()
-    mq135 = models.IntegerField()
-
-    def __str__(self):
-        return str(self.reading_id)
-    
-class BoxThree(models.Model):
-    reading_id = models.AutoField(primary_key=True)
-    timestamp = models.DateTimeField(auto_now=True)
-    mq2 = models.IntegerField()
-    mq3 = models.IntegerField()
-    mq7 = models.IntegerField()
-    mq9 = models.IntegerField()
-    mq135 = models.IntegerField()
-
-    def __str__(self):
-        return str(self.reading_id)
-    
-class BoxFour(models.Model):
+# This class is used to store each sensor reading
+class SensorData(models.Model):
     reading_id = models.AutoField(primary_key=True)
     timestamp = models.DateTimeField(auto_now=True)
     mq2 = models.IntegerField()
