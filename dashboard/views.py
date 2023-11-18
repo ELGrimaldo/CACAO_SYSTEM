55515440168c84e2de3b96f9929bd417b99670be
Rikiya Yamamoto <rikiyaeduarteyamamoto@gmail.com>
2023-11-18 15:36:41 +0800

    Make it only one function where you can request box data
    Since only one box will be used for fermenting again

--- dashboard/views.py
+++ dashboard/views.py
@@ -50,66 +50,54 @@ def receive_data(request):
     else:
         return HttpResponse("[FROM SERVER] Invalid request method")
 
-def get_box_one_data(request, start, end):
-    response = {}
-    data = BoxOne.objects.filter(reading_id__gte=start, reading_id__lte=end).order_by('timestamp')
-    if data.count() < 10:
-        print(f"[Box 1] {data.count()}")
-        response['status'] = 'INCOMPLETE'
-    else:
-        response.update(data.aggregate(
-            mq2_ave = Avg('mq2'),
-            mq3_ave = Avg('mq3'),
-            mq7_ave = Avg('mq7'),
-            mq9_ave = Avg('mq9'),
-            mq135_ave = Avg('mq135')))
-        response['status'] = 'COMPLETE'
-    return JsonResponse(response)
+def get_box_data(request, start, end):
+    """ Returns the moving average data of every 3000 rows and the prediction of fermentation degree of cacao beans """
 
-def get_box_two_data(request, start, end):
-    response = {}
-    data = BoxTwo.objects.filter(reading_id__gte=start, reading_id__lte=end).order_by('timestamp')
-    if data.count() < 10:
-        print(f"[Box 2] {data.count()}")
-        response['status'] = 'INCOMPLETE'
-    else:
-        response.update(data.aggregate(
-            mq2_ave = Avg('mq2'),
-            mq3_ave = Avg('mq3'),
-            mq7_ave = Avg('mq7'),
-            mq9_ave = Avg('mq9'),
-            mq135_ave = Avg('mq135')))
-        response['status'] = 'COMPLETE'
-    return JsonResponse(response)
+    # To store the JSON response
+    response = {
+        'status': 'INCOMPLETE' # Initially, there is no data and prediction result, so the status should be incomplete yet.
+    }
 
-def get_box_three_data(request, start, end):
-    response = {}
-    data = BoxThree.objects.filter(reading_id__gte=start, reading_id__lte=end).order_by('timestamp')
-    if data.count() < 10:
-        print(f"[Box 3] {data.count()}")
-        response['status'] = 'INCOMPLETE'
-    else:
-        response.update(data.aggregate(
-            mq2_ave = Avg('mq2'),
-            mq3_ave = Avg('mq3'),
-            mq7_ave = Avg('mq7'),
-            mq9_ave = Avg('mq9'),
-            mq135_ave = Avg('mq135')))
-        response['status'] = 'COMPLETE'
-    return JsonResponse(response)
+    # Should read 3000 contiguous rows of data
+    # Start and end query indices comes from the argument passed in the URL request parameter by streamlit
+    data = SensorData.objects.filter(reading_id__gte=start, reading_id__lte=end).order_by('timestamp')
 
-def get_box_four_data(request, start, end):
-    response = {}
-    data = BoxFour.objects.filter(reading_id__gte=start, reading_id__lte=end).order_by('timestamp')
-    if data.count() < 10:
-        print(f"[Box 4] {data.count()}")
-        response['status'] = 'INCOMPLETE'
-    else:
-        response.update(data.aggregate(
-            mq2_ave = Avg('mq2'),
-            mq3_ave = Avg('mq3'),
-            mq7_ave = Avg('mq7'),
-            mq9_ave = Avg('mq9'),
-            mq135_ave = Avg('mq135')))
+    # If the row count is 3000, calculate the average of each column.
+    # Then, use the average values to predict the fermentation degree of cacao beans
+    # and store them in a JSON and return it as response.
+    if data.count() == 3000:
+
+        # Calculate the average of each column (mq2 to mq135)
+        avgs = data.aggregate(
+                mq2_ave = Avg('mq2'),
+                mq3_ave = Avg('mq3'),
+                mq7_ave = Avg('mq7'),
+                mq9_ave = Avg('mq9'),
+                mq135_ave = Avg('mq135'))
+        
+        # Store it in JSON
+        response.update(avgs)
+        
+        # Use the averages in a data frame (so that it can be used for prediction)
+        data = pd.DataFrame({
+            'mq2': [response['mq2_ave']],
+            'mq3': [response['mq3_ave']],
+            'mq7': [response['mq7_ave']],
+            'mq9': [response['mq9_ave']],
+            'mq135': [response['mq135_ave']],
+        })
+
+        # Pass in the data frame to the prediction function
+        prediction = predict(data)
+
+        # Store the result in JSON
+        response['prediction'] = int(prediction[0][0])
+
+        # Change the status to complete now that there is data and prediction result
         response['status'] = 'COMPLETE'
+    
+    # If the row count is not exactly 3000 rows, just print the row count.
+    else:
+        print(f"[SERVER LOG] Data count: {data.count()}")
+    
     return JsonResponse(response)
